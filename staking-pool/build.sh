ca23980618a319a2cdec28b5c7f3fe18550bb95a
Evgeny Kuzyakov <ek@nearprotocol.com>
2020-05-06 17:23:58 -0700

    Remove old staking pool contract and rename

--- staking-pool/build.sh
+++ staking-pool/build.sh
@@ -2,4 +2,4 @@
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/staking_pool_with_shares.wasm ./res/
+cp target/wasm32-unknown-unknown/release/staking_pool.wasm ./res/
