0e07b5d8b8e6eeb569e227d97e737bad6932a24a
Bowen Wang <bowenwang1996@uchicago.edu>
2020-05-26 13:39:51 -0700

    rename and update readme

--- staking-pool/src/lib.rs
+++ staking-pool/src/lib.rs
@@ -428,7 +428,7 @@ impl StakingContract {
     }
 
     /// Returns the staking public key
-    pub fn get_stake_public_key(&self) -> Base58PublicKey {
+    pub fn get_staking_key(&self) -> Base58PublicKey {
         self.stake_public_key.clone().try_into().unwrap()
     }
 
