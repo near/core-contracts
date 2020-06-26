2c82bb3e91b8f38e82e28ecb65f3898df10cdf49
Evgeny Kuzyakov <ek@nearprotocol.com>
2020-06-26 10:00:12 -0700

    Update voting action

--- voting/src/lib.rs
+++ voting/src/lib.rs
@@ -73,7 +73,8 @@ impl VotingContract {
         }
     }
 
-    /// Internal function to handle vote and withdraw.
+    /// Method for validators to vote or withdraw the vote.
+    /// Votes for if `is_vote` is true, or withdraws the vote if `is_vote` is false.
     pub fn vote(&mut self, is_vote: bool) {
         self.ping();
         if self.result.is_some() {
