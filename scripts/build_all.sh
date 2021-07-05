4f00a458ef07cc08bff4e09e0d0e6db4267bcd8a
chefsale <sandi@tenzen.io>
2021-07-05 16:32:17 +0200

    feat(buildkite): added initial core-contracts CI setup

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 set -ex
 
+CHECK=0
+
+# Loop through arguments and process them
+for arg in "$@"
+do
+    case $arg in
+        -c|--check)
+        CHECK=1
+        shift 
+        ;;
+    esac
+done
+
 # Note: `staking-pool` has to be built before `staking-pool-factory`
 for p in lockup multisig staking-pool staking-pool-factory voting whitelist
 do
  (cd ${p} && ./build.sh)
 done
+
+if [ $CHECK == 1 ] && [ "$(git diff --exit-code)" != 0 ] ; then
+	echo "Repository is dirty, please make sure you have committed all contract wasm files"
+	exit 1
+fi
