19445eedc6ec9299bcf98f85d22b07c8a99b198c
chefsale <sandi@tenzen.io>
2021-07-05 16:32:17 +0200

    feat(buildkite): added initial core-contracts CI setup

---
+++ scripts/test_all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -ex
+
+# Note: `staking-pool` has to be built before `staking-pool-factory`
+while read -r contract
+do
+ 	(cd ${contract} && RUSTFLAGS='-D warnings' cargo test)
+done < CONTRACTS
