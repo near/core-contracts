33a8947a9bba66800d76634f1c228a2b99824219
Sandi Fatic <sandi.fatic@gmail.com>
2021-07-13 18:57:00 +0200

    feat(buildkite): added a build_all_docker script and --check flag for the artifacts in the CI pipeline (#166)

--- scripts/build_all_docker.sh
+++ scripts/build_all_docker.sh
@@ -17,7 +17,8 @@ done
 # Note: `staking-pool` has to be built before `staking-pool-factory`
 jq -c '.[]' scripts/contracts.json | while read i; do
   CONTRACT_DIR=$(echo $i | jq -r '.contract_dir')
- (cd $CONTRACT_DIR && ./build.sh)
+  CONTRACT_NAME=$(echo $i | jq -r '.contract_name')
+	(./scripts/build_docker.sh $CONTRACT_DIR $CONTRACT_NAME)
 done
 
 if [ $CHECK == 1 ] && [ ! -z "$(git diff --exit-code)" ] ; then
