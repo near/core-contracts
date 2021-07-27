6e12688365f2990c4e31d1469ef1b7f715e65226
austinabell <austinabell8@gmail.com>
2021-07-27 12:55:26 -0400

    update to 4.0.0-pre.1 to remove duplicate code

--- generic-factory/src/lib.rs
+++ generic-factory/src/lib.rs
@@ -1,18 +1,10 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-#[cfg(not(target_arch = "wasm32"))]
-use near_sdk::env::BLOCKCHAIN_INTERFACE;
-use near_sdk::json_types::{Base58CryptoHash, Base58PublicKey, Base64VecU8, U128};
+use near_sdk::json_types::{Base58CryptoHash, Base64VecU8, U128};
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, near_bindgen, AccountId, Balance, CryptoHash};
-
-#[global_allocator]
-static ALLOC: near_sdk::wee_alloc::WeeAlloc<'_> = near_sdk::wee_alloc::WeeAlloc::INIT;
+use near_sdk::{env, near_bindgen, sys, AccountId, Balance, CryptoHash, Gas, PublicKey};
 
 /// This gas spent on the account creation and contract deployment, the rest goes to the `new` call.
-const GAS_FOR_DEPLOY: u64 = 10_000_000_000_000;
-
-#[cfg(not(target_arch = "wasm32"))]
-const BLOCKCHAIN_INTERFACE_NOT_SET_ERR: &str = "Blockchain interface not set.";
+const GAS_FOR_DEPLOY: Gas = Gas(10_000_000_000_000);
 
 #[near_bindgen]
 #[derive(BorshSerialize, BorshDeserialize)]
@@ -28,9 +20,9 @@ impl Default for GenericFactory {
 #[serde(crate = "near_sdk::serde")]
 #[serde(untagged)]
 pub enum AccessKey {
-    FullAccessKey(Base58PublicKey),
+    FullAccessKey(PublicKey),
     FunctionCall {
-        public_key: Base58PublicKey,
+        public_key: PublicKey,
         receiver_id: AccountId,
         method_names: Vec<String>,
         allowance: U128,
@@ -63,8 +55,6 @@ impl GenericFactory {
 #[no_mangle]
 pub extern "C" fn store() {
     env::setup_panic_hook();
-    //* This can be removed in the future when wasm32 doesn't go through this interface
-    env::set_blockchain_interface(Box::new(near_blockchain::NearBlockchain {}));
     unsafe {
         // Load input into register 0.
         sys::input(0);
@@ -106,7 +96,6 @@ pub(crate) fn internal_create(
 ) {
     let attached_deposit = env::attached_deposit();
 
-    #[cfg(target_arch = "wasm32")]
     unsafe {
         // Load input (wasm code) into register 0.
         sys::storage_read(hash.len() as _, hash.as_ptr() as _, 0);
@@ -123,10 +112,11 @@ pub(crate) fn internal_create(
         for access_key in access_keys.iter() {
             match access_key {
                 AccessKey::FullAccessKey(public_key) => {
+                    let public_key = public_key.as_bytes();
                     sys::promise_batch_action_add_key_with_full_access(
                         promise_id,
-                        public_key.0.len() as _,
-                        public_key.0.as_ptr() as _,
+                        public_key.len() as _,
+                        public_key.as_ptr() as _,
                         0,
                     )
                 }
@@ -135,17 +125,21 @@ pub(crate) fn internal_create(
                     allowance,
                     receiver_id,
                     method_names,
-                } => sys::promise_batch_action_add_key_with_function_call(
-                    promise_id,
-                    public_key.0.len() as _,
-                    public_key.0.as_ptr() as _,
-                    0,
-                    &allowance.0 as *const Balance as _,
-                    receiver_id.len() as _,
-                    receiver_id.as_ptr() as _,
-                    method_names.len() as _,
-                    method_names.as_ptr() as _,
-                ),
+                } => {
+                    let receiver_id = receiver_id.as_str();
+                    let public_key = public_key.as_bytes();
+                    sys::promise_batch_action_add_key_with_function_call(
+                        promise_id,
+                        public_key.len() as _,
+                        public_key.as_ptr() as _,
+                        0,
+                        &allowance.0 as *const Balance as _,
+                        receiver_id.len() as _,
+                        receiver_id.as_ptr() as _,
+                        method_names.len() as _,
+                        method_names.as_ptr() as _,
+                    )
+                }
             }
             sys::promise_batch_action_deploy_contract(promise_id, u64::MAX as _, 0);
         }
@@ -161,74 +155,9 @@ pub(crate) fn internal_create(
                 args.len() as _,
                 args.as_ptr() as _,
                 0,
-                attached_gas,
+                attached_gas.0,
             );
             // todo: add callback to handle.
         }
     }
-
-    //* This is duplicated code to support going through BLOCKCHAIN_INTERFACE for non-wasm target
-    // TODO this should not be needed in the future and only has a small impact on contract size
-    #[cfg(not(target_arch = "wasm32"))]
-    BLOCKCHAIN_INTERFACE.with(|b| unsafe {
-        let borrowed = b.borrow();
-        let interface = borrowed.as_ref().expect(BLOCKCHAIN_INTERFACE_NOT_SET_ERR);
-        // Load input (wasm code) into register 0.
-        interface.storage_read(hash.len() as _, hash.as_ptr() as _, 0);
-        // todo: handle missing hash to return attached deposit.
-        // schedule a Promise tx to account_id
-        let promise_id =
-            interface.promise_batch_create(account_id.len() as _, account_id.as_ptr() as _);
-        // create account first.
-        interface.promise_batch_action_create_account(promise_id);
-        // transfer attached deposit.
-        interface.promise_batch_action_transfer(promise_id, &attached_deposit as *const u128 as _);
-        // deploy contract (code is taken from register 0).
-        interface.promise_batch_action_deploy_contract(promise_id, u64::MAX as _, 0);
-        // add access keys.
-        for access_key in access_keys.iter() {
-            match access_key {
-                AccessKey::FullAccessKey(public_key) => interface
-                    .promise_batch_action_add_key_with_full_access(
-                        promise_id,
-                        public_key.0.len() as _,
-                        public_key.0.as_ptr() as _,
-                        0,
-                    ),
-                AccessKey::FunctionCall {
-                    public_key,
-                    allowance,
-                    receiver_id,
-                    method_names,
-                } => interface.promise_batch_action_add_key_with_function_call(
-                    promise_id,
-                    public_key.0.len() as _,
-                    public_key.0.as_ptr() as _,
-                    0,
-                    &allowance.0 as *const Balance as _,
-                    receiver_id.len() as _,
-                    receiver_id.as_ptr() as _,
-                    method_names.len() as _,
-                    method_names.as_ptr() as _,
-                ),
-            }
-            interface.promise_batch_action_deploy_contract(promise_id, u64::MAX as _, 0);
-        }
-        if method_name.is_some() && args.is_some() {
-            // call this_contract.<method_name>(<args>) with remaining gas.
-            let attached_gas = env::prepaid_gas() - env::used_gas() - GAS_FOR_DEPLOY;
-            let method_name = method_name.unwrap();
-            let args = args.unwrap();
-            interface.promise_batch_action_function_call(
-                promise_id,
-                method_name.len() as _,
-                method_name.as_ptr() as _,
-                args.len() as _,
-                args.as_ptr() as _,
-                0,
-                attached_gas,
-            );
-            // todo: add callback to handle.
-        }
-    });
 }
