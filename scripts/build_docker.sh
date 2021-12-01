78a09dd08d148f6d87f7b7b2e2fd40b2c727110e
Illia Polosukhin <ilblackdragon@gmail.com>
2021-12-01 08:37:40 +0000

    Rebuild all contracts with the same toolchain version

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -20,7 +20,7 @@ docker create \
 fi
 
 docker start build_$NAME
-docker exec build_$NAME /bin/bash -c "cargo build --target wasm32-unknown-unknown --release"
+docker exec build_$NAME /bin/bash -c "rustup toolchain install stable-2020-10-08; rustup default stable-2020-10-08; rustup target add wasm32-unknown-unknown; cargo build --target wasm32-unknown-unknown --release"
 
 mkdir -p res
 cp $NAME/target/wasm32-unknown-unknown/release/$CONTRACT_WASM_NAME.wasm $NAME/res/$CONTRACT_WASM_NAME.wasm
